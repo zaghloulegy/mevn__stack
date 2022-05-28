7b7dbbd3d75cf0993382c64d79c94ab44c34b6af
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-28 19:09:37 +0100

    using tailwind css

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Club />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Club from "./components/Club.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Club
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ client/src/components/Club.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="Club">
+    <div class="container mx-auto mt-5 bg-gray-700 text-gray-300">
+      <h2 class="text-2xl p-3">UCL CLUBS</h2>
+      <div class="forms flex flex-col w-full items-center">
+        <div class="box flex flex-col w-1/2 mb-3">
+          <label for="name" class="text-sm"> Club Name </label>
+          <input
+            type="text"
+            id="name"
+            class="bg-gray-800 py-1 rounded focus outline-none px-2"
+          />
+        </div>
+        <div class="box flex flex-col w-1/2 mb-3">
+          <label for="League" class="text-sm"> League </label>
+          <input
+            type="text"
+            id="League"
+            class="bg-gray-800 py-1 rounded focus outline-none px-2"
+          />
+        </div>
+        <button class="bg-gray-200 px-5 mb-5 rounded text-gray-900">
+          Submit
+        </button>
+      </div>
+    </div>
+    <div class="all-clubs mt-4 container mx-auto">
+      <ul class="list space-y-2 flex inline-flex flex-wrap justify-between w-full">
+        <li
+          class="text-gray-300 w-1/3 bg-gray-800 list-item shadow hover:bg-gray-700 transition-all transition-colors"
+        >
+          <div
+            class="inside-list flex justify-between flex-row border-b border-solid border-indigo-900 mb-2"
+          >
+            <p class="w-1/2">Club Name</p>
+            <p class="w-1/2">League</p>
+          </div>
+          <button class="bg-red-600 py-1 px-2 rounded">Delete</button>
+          <button class="bg-blue-600 py-1 px-2 rounded">Update</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style scoped></style>
